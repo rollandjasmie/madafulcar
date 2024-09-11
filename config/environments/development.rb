23e652bb21201e8e59d2e41ad610ef1ceaa97104
R Rolland <52662945+rollandjasmie@users.noreply.github.com>
2024-09-11 17:47:07 +0300

    Update development.rb host_name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.hosts << ENV['DEVELOPMENT_HOSTS']
 
   config.action_mailer.delivery_method = :letter_opener
   
